7f0c52e4d4cb36d95b2d5cf04e14c273e9ae4d88
BrandoonRH <aeroatlas.51@gmail.com>
2026-08-11 11:53:46 -0600

    Sección 12: Creando Campos Personalizados en la WP REST API

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
 import { z } from "zod";
 
+const imageSchema = z.object({
+  url: z.string(),
+  width: z.number(),
+  height: z.number(),
+});
+
+const featuredImagesSchemas = z.object({
+  thumbnail: imageSchema,
+  medium: imageSchema,
+  medium_large: imageSchema,
+  large: imageSchema,
+  full: imageSchema,
+});
+
 export const BaseWPSchema = z.object({
   id: z.number(),
   title: z.object({
@@ -11,4 +25,5 @@ export const BaseWPSchema = z.object({
   acf: z.object({
     subtitle: z.string(),
   }),
+  feature_images: featuredImagesSchemas
 });
